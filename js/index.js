558ec2793d6ead13ad043fe97508d3f824dd25f3
Walter Larrea <walterlarrea@live.com>
2022-08-15 00:10:35 -0300

    Revert "Desafiate1 - Tercer intento Iniciar con Google"
    This reverts commit ad5e6c4a6096f8f8a4f95edfea95fb9f641cdd18.

--- js/index.js
+++ js/index.js
@@ -2,27 +2,7 @@ document.addEventListener("DOMContentLoaded", function(){
     //navega al formulario de login en caso que no esté logeado
     if ( sessionStorage.getItem("loggedIn") === null || sessionStorage.getItem("loggedIn") === false ) {
         window.location = "login.html";
-    } else {
-        let navigationBar = document.getElementsByTagName('ul')
-        let logOutOption = document.createElement('li');
-        let logOutLink = document.createElement('a');
-
-        logOutLink.innerHTML = "Cerrar sesión";
-        logOutLink.id = "full-log-out";
-        logOutLink.classList.add("nav-link");
-        logOutOption.classList.add("nav-item");
-
-        logOutOption.appendChild(logOutLink);
-        navigationBar[0].appendChild(logOutOption);
     }
-
-    document.getElementById("full-log-out").addEventListener("click", function() {
-        sessionStorage.setItem("loggedIn", false);
-        let auth2 = gapi.auth2.getAuthInstance();
-            auth2.signOut().then(function () {
-            console.log('User signed out.');
-    });
-    })
 
     document.getElementById("autos").addEventListener("click", function() {
         localStorage.setItem("catID", 101);
