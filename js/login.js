ad5e6c4a6096f8f8a4f95edfea95fb9f641cdd18
Walter Larrea <walterlarrea@live.com>
2022-08-14 23:55:30 -0300

    Desafiate1 - Tercer intento Iniciar con Google

--- js/login.js
+++ js/login.js
@@ -23,6 +23,15 @@ document.addEventListener("DOMContentLoaded", function(){
     })
 });
 
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    loginSucces()
+}
+
 function loginSucces() {
     sessionStorage.setItem("loggedIn", true);
     window.location = "index.html";
