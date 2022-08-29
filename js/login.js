3cf7a0a0913a6e58d2f793ab9fcabb67aefad5f2
Walter Larrea <walterlarrea@live.com>
2022-08-28 22:02:26 -0300

    Entrega 1 - Fin Avance Desafío

--- js/login.js
+++ js/login.js
@@ -41,10 +41,10 @@ document.addEventListener("DOMContentLoaded", function(){
         console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
     }
     // Script de google SignOut
-    document.getElementById('googleSignOut').addEventListener("click", function() {
-        let auth2 = gapi.auth2.getAuthInstance();
-        auth2.signOut().then(function () {
-        console.log('User signed out.');
-        });
-    })
+    // document.getElementById('googleSignOut').addEventListener("click", function() {
+    //     let auth2 = gapi.auth2.getAuthInstance();
+    //     auth2.signOut().then(function () {
+    //     console.log('User signed out.');
+    //     });
+    // })
 });
