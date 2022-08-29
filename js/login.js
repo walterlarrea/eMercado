d43b3fda05134676b83e7337d0cfc334d1fbc518
Walter Larrea <walterlarrea@live.com>
2022-08-28 22:18:54 -0300

    Entrega 1 - DA

--- js/login.js
+++ js/login.js
@@ -31,20 +31,21 @@ document.addEventListener("DOMContentLoaded", function(){
     passInput.addEventListener("input", function () {
         passInput.classList.remove("is-invalid");
     })
-
-    // Script de google SignIn
-    // function onSignIn(googleUser) {
-    //     let profile = googleUser.getBasicProfile();
-    //     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    //     console.log('Name: ' + profile.getName());
-    //     console.log('Image URL: ' + profile.getImageUrl());
-    //     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-    // }
-    // Script de google SignOut
-    // document.getElementById('googleSignOut').addEventListener("click", function() {
-    //     let auth2 = gapi.auth2.getAuthInstance();
-    //     auth2.signOut().then(function () {
-    //     console.log('User signed out.');
-    //     });
-    // })
 });
+
+
+    //Script de google SignIn
+    function onSignIn(googleUser) {
+        let profile = googleUser.getBasicProfile();
+        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+        console.log('Name: ' + profile.getName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    }
+    //Script de google SignOut
+    document.getElementById('googleSignOut').addEventListener("click", function() {
+        let auth2 = gapi.auth2.getAuthInstance();
+        auth2.signOut().then(function () {
+        console.log('User signed out.');
+        });
+    })
