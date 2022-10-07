8e686b5f948349f82421990a20db5de31a7d663b
Walter Larrea <walterlarrea@live.com>
2022-10-06 22:03:37 -0300

    actualizo con validación Google oAuth

--- js/login.js
+++ js/login.js
@@ -1,5 +1,5 @@
-function checkLoginRedirect () {
-    if ( !(localStorage.getItem("loggedUser") === null || localStorage.getItem("loggedUser") === "" )) {
+function checkLoginRedirect() {
+    if (!(localStorage.getItem("loggedUser") === null || localStorage.getItem("loggedUser") === "")) {
         alert("Cierre su sesión actual para poder iniciar una nueva");
         window.location = "index.html";
     }
@@ -14,6 +14,7 @@ function loginSucces(userEmail) {
 // Google sign in and redirect function
 function onSignIn(response) {
     const responsePayload = jwt_decode(response.credential);// Decodes the response received from google API
+    console.log(responsePayload);
 
     console.log("ID: " + responsePayload.sub);
     console.log('Full Name: ' + responsePayload.name);
@@ -22,21 +23,26 @@ function onSignIn(response) {
     console.log("Image URL: " + responsePayload.picture);
     console.log("Email: " + responsePayload.email);
 
+    // Validate positive response
+    if (!responsePayload.email_verified) {
+        alert("Hubo un error al iniciar su sesión con Goole, vuelve a intentarlo.")
+        return
+    }
     loginSucces(responsePayload.email);
 }
 
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
     const emailInput = document.getElementById("email");
     const passInput = document.getElementById("password");
 
-    passInput.addEventListener("keypress", function(e) {
+    passInput.addEventListener("keypress", function (e) {
         if (e.key === "Enter") {
-          document.getElementById("loginBtn").click();
+            document.getElementById("loginBtn").click();
         }
     });
-    
-    document.getElementById("loginBtn").addEventListener("click", function() {
-        if ( emailInput.value != "" && passInput.value != "" ) {// Login only if the user introduced both email and password
+
+    document.getElementById("loginBtn").addEventListener("click", function () {
+        if (emailInput.value != "" && passInput.value != "") {// Login only if the user introduced both email and password
             loginSucces(emailInput.value);
         } else {
             if (!emailInput.validity.valid) {
